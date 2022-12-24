28da80fa17c59c84a3cba2d8eb414bb8e78ffa02
markovicf <filipm@elfak.rs>
2022-12-24 14:50:02 +0100

    cofi_model_first

---
+++ ForumApp/ForumApp/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ForumApp.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        //fk
+
+        //[JsonIgnore]
+        //public List<Comment> Comments { get; set; }
+        //public List<Like_Post> Likes {get; set;}
+        //public List<Dislike_Post> Dislikes {get; set;}
+        //public Topic Topic { get; set; }
+        //public User User { get; set; }
+    }
+}
--- ForumApp/ForumApp/Models/User.cs
+++ ForumApp/ForumApp/Models/User.cs
@@ -2,12 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 
 namespace ForumApp.Models
 {
     public class User
     {
-        
+       public int ID { get; set; }
+        public string Username { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+
+        //fk
+
+        [JsonIgnore]
+        public List<Post> Posts { get; set; }
+        [JsonIgnore]
+        public List<Community> Communities { get; set; }
     }
 }
